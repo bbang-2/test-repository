a1abcfa45be09837aeeba9ae923f5c885cf91876
bbang-2 <yys799@nate.com>
2025-05-13 17:35:17 +0900

    merge-example

--- src/main/java/com/example/testback/common/ResponseMessage.java
+++ src/main/java/com/example/testback/common/ResponseMessage.java
@@ -4,5 +4,5 @@ public class ResponseMessage {
     public static final String SUCCESS = "Success";
 
     public static final String NOT_EXISTS_RESTAURANT = "Restaurant not found with id: ";
-    public static final String NOT_EXISTS_MENU = "Menu not found with id: ";
+//    public static final String NOT_EXISTS_MENU = "Menu not found with id: ";
 }
